25cd93b8f34efa19c7cb5037c42af33a5ee4cd75
Sue Roh <chopsueshi@yahoo.com>
2020-02-10 11:37:55 -0500

    Before troubleshooting

--- lcd.ino
+++ lcd.ino
@@ -1,14 +1,11 @@
 /*
   LiquidCrystal Library - Hello World
-
  Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
  library works with all LCD displays that are compatible with the
  Hitachi HD44780 driver. There are many of them out there, and you
  can usually tell them by the 16-pin interface.
-
  This sketch prints "Hello World!" to the LCD
  and shows the time.
-
   The circuit:
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
@@ -22,7 +19,6 @@
  * 10K resistor:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
-
  Library originally added 18 Apr 2008
  by David A. Mellis
  library modified 5 Jul 2009
@@ -31,9 +27,7 @@
  by Tom Igoe
  modified 22 Nov 2010
  by Tom Igoe
-
  This example code is in the public domain.
-
  http://www.arduino.cc/en/Tutorial/LiquidCrystal
  */
 
@@ -41,15 +35,23 @@
 #include <LiquidCrystal.h>
 
 String textArr[] = {
-   "JuneJuneJuneJuneJuneJune",
-   "Adrian Napier",
-   "Jasen Jacques",
-   "Erika Coronel",
-   "Ben Marshall",
+   "MTA criminalizespoverty           ",
+   "Go! We paid:    $249,000,000   ",
+   "Don't snitch,   swipe               ",
+   "Arrested for    $2.75             "
 };
+
+#define LIGHT_SENSOR_PIN 0
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int photocellReading;
+int period = 7000;
+bool isTriggered = false;
+int lightThreshold = 1;
+int idx = 0;
+unsigned long time_now = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -57,21 +59,39 @@ void setup() {
 }
 
 void loop() {
+  photocellReading = analogRead(LIGHT_SENSOR_PIN); 
   int len = sizeof(textArr) / sizeof(textArr[0]);
-  for (int idx = 0; idx < len; idx++) {
-    lcd.clear();
+      Serial.println(photocellReading);
+
+
+  if (photocellReading > lightThreshold && !isTriggered) {
+    isTriggered = true;
+  }
+  
+  if (isTriggered) {
+    int lengthOfArr = textArr[idx].length() > 16 ? 32 : textArr[idx].length();
     float space = (16 - textArr[idx].length()) / 2;
     int offset = round(space);
     lcd.setCursor(offset, 0); // Centers the text
     lcd.print(textArr[idx]);
     if (textArr[idx].length() > 16) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
       String text = textArr[idx];
-      String str = text.substring(16, textArr[idx].length());
+
+      String str1 = text.substring(0, 16);
+
+      lcd.print(str1);
+      String str = text.substring(16, 32);
       float spaceSecond = (16 - str.length()) / 2;
       int offsetSecond = round(spaceSecond);
       lcd.setCursor(offsetSecond, 1);
       lcd.print(str);
     }
     delay(7000);
+    idx = (idx == len - 1) ? 0 : idx + 1;
+
+    isTriggered = false;
+    lcd.clear();
   }
 }
